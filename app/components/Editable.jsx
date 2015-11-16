456d0a9505436396879d384a9774f2a0383af8a2
Harro van der Klauw <hvdklauw@gmail.com>
2015-11-16 11:49:01 +0100

    First lane implementation

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Note extends React.Component {
+export default class Editable extends React.Component {
 	constructor(props) {
 		super(props);
 
@@ -8,34 +8,35 @@ export default class Note extends React.Component {
 		this.checkEnter = this.checkEnter.bind(this);
 		this.edit = this.edit.bind(this);
 		this.renderEdit = this.renderEdit.bind(this);
-		this.renderTask = this.renderTask.bind(this);
+        this.renderValue = this.renderValue.bind(this);
 
 		this.state = {
 			editing: false
 		};
 	}
 	render() {
+        const {value, onEdit, ...props} = this.props;
 		const editing = this.state.editing;
 
 		return (
-			<div>
-				{editing ? this.renderEdit() : this.renderTask()}
+			<div {...props}>
+				{editing ? this.renderEdit() : this.renderValue()}
 			</div>
 		);
 	}
 	renderEdit() {
 		return <input type="text"
 						autoFocus={true}
-						defaultValue={this.props.task}
+						defaultValue={this.props.value}
 						onBlur={this.finishEdit}
 						onKeyPress={this.checkEnter} />;
 	}
-	renderTask() {
+	renderValue() {
 		const onDelete = this.props.onDelete;
 
 		return (
 			<div onClick={this.edit}>
-				<span className="tasl">{this.props.task}</span>
+				<span className="value">{this.props.value}</span>
 				{onDelete ? this.renderDelete() : null }
 			</div>
 		);
