5c5ef264d32579621d7f5b5a0e4f6296f8a4ee84
Harro van der Klauw <hvdklauw@gmail.com>
2015-11-16 11:01:38 +0100

    Initial commit, finished the notes app.

---
+++ app/libs/alt.js
@@ -0,0 +1,8 @@
+import Alt from 'alt';
+// See: https://github.com/goatslacker/alt-devtool
+//import chromeDebug from 'alt/utils/chromeDebug';
+
+const alt = new Alt();
+//chromeDebug(alt);
+
+export default alt;
